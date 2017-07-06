5e7dedff7e38cd033badc76438ab20b3e7580d36
Gavin Lee <sz110010@gmail.com>
2017-07-07 00:52:18 +0800

    add pic

--- styles.js
+++ styles.js
@@ -2,3 +2,28 @@ $(document).ready(function(){
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
 });
+
+$("#p1").click(function () {
+   $("#ps1").modal('open');
+});
+$("#p2").click(function () {
+   $("#ps2").modal('open');
+});
+$("#p3").click(function () {
+   $("#ps3").modal('open');
+});
+$("#p4").click(function () {
+   $("#ps4").modal('open');
+});
+$("#p5").click(function () {
+   $("#ps5").modal('open');
+});
+$("#p6").click(function () {
+   $("#ps6").modal('open');
+});
+
+$('.maparea').mouseover(function() {
+    $('.maparea').addClass('shine');
+}).mouseout(function(){
+    $('.maparea').removeClass('shine');
+});
